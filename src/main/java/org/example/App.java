7c7c33bc8e9551c9492945a6ca910e867ffd0b5f
hCri02 <c.hanu@itsrizzoli.it>
2022-05-16 12:37:25 +0200

    Implements JSON

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpServer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class App 
@@ -14,10 +19,31 @@ public class App
 
     static ArrayList<Product> products = new ArrayList<>();
     static int portNumber = 1234;
+    static double cheapest = 100000;
+    static Gson gson = new Gson();
+    static String json;
 
     public static void main( String[] args )
     {
+
+        // HTTPS Server
+
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(8000), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //server.createContext("/applications/myapp", new MyHandler());
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+
+        // Creazione della lista
+
         buildProductList();
+
+        // Implementazione del server
 
         System.out.println("Server started!");
 
@@ -52,6 +78,8 @@ public class App
             e.printStackTrace();
         }
 
+        // switch delle query
+
         while (true){
             String s = "";
             try {
@@ -59,25 +87,47 @@ public class App
                     if (!((s = in.readLine()) != null))
                         break;
 
-                    switch (s) {
-                        case "cheaper":
-                            System.out.println("cheaper");
-                            break;
-                        case "all":
-                            System.out.println("all");
-                            break;
-                        case "all_sorted":
-                            System.out.println("all_sorted");
-                            break;
-                        default:
-                            System.out.println("Invalid option");
-                    }
+                    querySwitch(s);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+    }
+
+    // Funzioni
+
+    private static void querySwitch(String s) {
+        switch (s) {
+            case "cheapest":
+                for (Product product : products) {
+                    if (product.getCost() < cheapest) {
+                        cheapest = product.getCost();
+                        json = gson.toJson(product);
+                    }
+                }
+                System.out.println(json);
+                break;
+            case "all":
+                json = gson.toJson(products);
+                break;
+            case "all_sorted":
+                sortProductList();
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
+
+    private static void sortProductList() {
+        ArrayList<Product> orderList = new ArrayList<>();
+        for(Product o:products)
+            orderList.add(o);
+
+        //Collections.sort(orderList);
+        json = gson.toJson(orderList);
+        System.out.println(json);
     }
 
     static void buildProductList() {
