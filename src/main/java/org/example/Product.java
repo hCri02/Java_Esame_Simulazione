61869c0dddc974cd81f13ff100f77b77c12aa905
hCri02 <c.hanu@itsrizzoli.it>
2022-05-16 11:42:34 +0200

    Getter & Setter

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,16 +1,47 @@
 package org.example;
 
-public class Product {
+public class Product{
     int id;
     String name;
     double cost;
     int quantity;
 
-
     public Product(int id, String name, double cost, int quantity) {
         this.id = id;
         this.name = name;
         this.cost = cost;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
