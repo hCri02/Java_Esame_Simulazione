a297f8d79be966740545f582c096c88a682f3fca
hCri02 <c.hanu@itsrizzoli.it>
2022-05-16 09:49:16 +0200

    Create ProductClass

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Product {
+    int id;
+    String name;
+    double cost;
+    int quantity;
+
+
+    public Product(int id, String name, double cost, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+
+}
