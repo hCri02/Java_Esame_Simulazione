14998102fdc53c5587891279cdcb7bb044be2543
hCri02 <c.hanu@itsrizzoli.it>
2022-05-16 09:31:29 +0200

    Create ProductList

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    static ArrayList<Product> products = new ArrayList<>();
+
     public static void main( String[] args )
     {
+        buildProductList();
+    }
 
+    static void buildProductList() {
+        products.add(new Product(36213,"Huawei Honor 8 BLACK",25.94, 6));
+        products.add(new Product(36214,"Huawei Honor 8 RED",26.94, 1));
+        products.add(new Product(36215,"Apple IPhone 13 RED",1226.94, 10));
+        System.out.println(products);
     }
 }
